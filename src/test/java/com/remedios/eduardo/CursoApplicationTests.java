e3399fec6a8ff65f58917fe8e45e53900bda537a
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2024-02-15 22:24:19 -0300

    first commit

---
+++ src/test/java/com/remedios/eduardo/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios.eduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
