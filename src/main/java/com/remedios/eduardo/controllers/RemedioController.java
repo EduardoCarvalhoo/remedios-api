5e79bdb013acc1117652772d9883597a72cab22b
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2024-02-17 01:50:39 -0300

    feat: adding and persisting information in the database

--- src/main/java/com/remedios/eduardo/controllers/RemedioController.java
+++ src/main/java/com/remedios/eduardo/controllers/RemedioController.java
@@ -1,18 +1,24 @@
 package com.remedios.eduardo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.remedios.eduardo.remedio.DadosCadastroRemedios;
+import com.remedios.eduardo.remedio.Remedio;
+import com.remedios.eduardo.remedio.RemedioRepository;
 
 @RestController
 @RequestMapping("/remedios")
 public class RemedioController {
+	
+	@Autowired
+	private RemedioRepository repository;
 
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroRemedios dados) {
-		System.out.println(dados);
+		repository.save(new Remedio(dados));
 	}
 }
